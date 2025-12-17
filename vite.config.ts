601bb7cd1ba2c630810e36293332200ab482c9dc
moshe1ch-kidi <moshe1.ch@gmail.com>
2025-12-17 23:41:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+mport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // זה מוודא שהנתיבים יהיו יחסיים ולא יישברו
 })
