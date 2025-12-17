9ff5bbd6a2fb5972e649176e1ed000b94a491d2d
moshe1ch-kidi <moshe1.ch@gmail.com>
2025-12-17 23:44:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-mport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // זה מוודא שהנתיבים יהיו יחסיים ולא יישברו
+  base: '', // הוספת השורה הזו פותרת את בעיית ה-404 של הקבצים
 })
